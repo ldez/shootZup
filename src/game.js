1f6fb4566754a37d640eb30d9cc267cac954a08c
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2015-04-26 14:59:30 +0200

    feat(*): un peu de commentaire

--- src/game.js
+++ src/game.js
@@ -1,6 +1,21 @@
 (function (window) {
     'use strict';
 
+    /**
+     * Jeu
+     *
+     * @param {Object} canvas           Canvas du jeu
+     * @param {Object} context2d        Context 2D du canvas
+     * @param {Object} gameState        Gestionnaire de l'état d'éxécution du jeu
+     * @param {Object} resources        Gestionnaire des images
+     * @param {Object} explosionManager Gestionnaire des explosions
+     * @param {Object} ennemiesManager  Gestionnaire des ennemies
+     * @param {Object} lasersManager    Gestionnaire des lasers
+     * @param {Object} background       Gestionnaire du fond du jeu
+     * @param {Object} player1          Vaisseau du joueur 1
+     * @param {Object} physicsP1        Moteur de physique du joueur 1
+     * @param {Object} controlsP1       Gestionnaire des touches du clavier du joueur 1
+     */
     function Game(canvas, context2d, gameState, resources, explosionManager, ennemiesManager, lasersManager, background, player1, physicsP1, controlsP1) {
         this.canvas = canvas;
         this.context2d = context2d;
@@ -20,20 +35,36 @@
         this.controlsP1 = controlsP1;
     }
 
+    /**
+     * Démarage du jeu
+     *
+     * @param {Object} scenario Scénario à lancer
+     */
     Game.prototype.startGame = function (scenario) {
         this.scenario = scenario;
         this.scoresP1 = 0;
         this.gameState.play();
         this.player1.startLoop(this.player1.FLY);
 
+        // lance de scenario des ennemies
         this.ennemiesManager.start(scenario).then(function (sequence) {
+            // définit la durée maximale du jeu.
             setTimeout(function () {
                 this.gameState.finished();
             }.bind(this), 3000);
         }.bind(this));
     };
 
+    /**
+     * Gestion des actions clavier d'un joueur
+     *
+     * @param {Object} control Gestionnaire des touches du clavier
+     * @param {Object} physics Moteur de physique
+     * @param {Object} player  Vaisseau du joueur
+     */
     Game.prototype.checkInputInGame = function (control, physics, player) {
+
+        // lance un shoot
         if (control.actions.SHOOT) {
             // création de 2 lasers
             this.lasersManager.shoot(physics);
@@ -41,12 +72,14 @@
             control.actions.SHOOT = false;
         }
 
+        // déplacement horizontal
         if (control.actions.LEFT && physics.canMoveLeft(player)) {
             physics.x -= 10;
         } else if (control.actions.RIGHT && physics.canMoveRight(player)) {
             physics.x += 10;
         }
 
+        // déplacement vertical
         if (control.actions.UP && physics.canMoveUp(player)) {
             physics.y -= 10;
         } else if (control.actions.DOWN && physics.canMoveDown(player)) {
@@ -54,6 +87,12 @@
         }
     };
 
+    /**
+     * Gestion du relancement du jeu
+     *
+     * @param {Object} control Gestionnaire des touches du clavier
+     * @param {Object} physics Moteur de physique
+     */
     Game.prototype.checkInputMenu = function (control, physics) {
         if (this.gameState.isFinished() && control.actions.START) {
             physics.reset();
@@ -62,36 +101,58 @@
         }
     };
 
+    /**
+     * Affichage des éléments du jeu
+     */
     Game.prototype.paintGame = function () {
 
+        // affichage du fond du jeu
         this.background.paint(this.context2d);
 
+        // Si le jeu est en cours
         if (this.gameState.isPlaying()) {
+            // gestion des touches et des actions associées
             this.checkInputInGame(this.controlsP1, this.physicsP1, this.player1);
 
+            // affichage des ennemies
             this.ennemiesManager.paintEnnemies(this.context2d);
 
+            // affichage des explosions des ennemies
             this.explosionManager.paint(this.context2d);
 
+            // affichage des bullets des ennemies
             this.ennemiesManager.paintBullets(this.context2d);
 
+            // affichage du vaisseau du joueur
             this.player1.paint(this.context2d, this.physicsP1.x, this.physicsP1.y);
 
+            // affichage des lasers des joueurs
             this.lasersManager.paint(this.context2d);
 
+            // affichage du score
             this.paintScores(this.context2d, this.scoresP1);
+            // calcul du nouveau score
             this.scoresP1 += this.physicsP1.detectCollisionOnEnnemies(this.ennemiesManager.ennemies, this.lasersManager.lasers, this.exploding);
 
+            // detection des collisions avec le vaisseau du joueur
             if (this.physicsP1.detectCollisionsOnPlayer(this.physicsP1.x, this.physicsP1.y, this.player1, this.ennemiesManager.bullets)) {
-                this.destroyPlayer(this.physicsP1);
+                this.destroyPlayer(this.physicsP1, this.player1);
             }
 
+            // déplacement des lasers des joueurs
             this.lasersManager.move();
 
-        } else if (this.gameState.isDead()) {
+        }
+        // si le joueur est mort
+        else if (this.gameState.isDead()) {
+
+            // affichage de explosion du vaisseau
             this.explosionManager.paint(this.context2d);
         } else {
+            // relancement du jeu sur action du joueur
             this.checkInputMenu(this.controlsP1, this.physicsP1);
+
+            // affichage de l'écran de fin
             this.paintEndScreen(this.context2d, this.scoresP1);
         }
 
@@ -99,10 +160,17 @@
     };
 
 
-    Game.prototype.destroyPlayer = function (physics) {
+    /**
+     * Destruction d'un joueur
+     *
+     * @param {Object} physics Moteur de physique
+     */
+    Game.prototype.destroyPlayer = function (physics, player) {
+        // Change l'état du jeu
         this.gameState.death();
-        this.player1.clearCurrentAnimation();
+        player.clearCurrentAnimation();
 
+        // création de l'explosion du vaisseau du joueur
         this.explosionManager.plaverExploded(physics, function () {
             setTimeout(function () {
                 this.gameState.finished();
@@ -110,12 +178,24 @@
         }.bind(this));
     };
 
+    /**
+     * Affichage du score
+     *
+     * @param {Object} context Context 2D du canvas
+     * @param {Number} score   Score du joueur
+     */
     Game.prototype.paintScores = function (context, score) {
         context.fillStyle = "red";
         context.font = "bold 16px lcd";
         context.fillText("score " + score, 10, 25);
     };
 
+    /**
+     * Affichage de l'écran de fin
+     *
+     * @param {Object} context Context 2D du canvas
+     * @param {Number} score   Score du joueur
+     */
     Game.prototype.paintEndScreen = function (context, score) {
         context.fillStyle = "red";
         context.font = "bold 24px lcd";
