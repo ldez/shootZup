e699932f3fc07dee1c8e50a97af339abf431b59e
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2015-04-28 21:28:50 +0200

    feat(player): choix aléatoire du vaisseau

--- src/player/playerFactory.js
+++ src/player/playerFactory.js
@@ -7,7 +7,29 @@
     }
 
     PlayerFactory.prototype.create = function () {
-        return new SpaceshipRed(this.physics.canvasWidth / 2, 600, this.resources);
+
+        var spaceship = null;
+        var number = this.getRandomInt(1, 3);
+
+        switch (number) {
+        case 1:
+            spaceship = new SpaceshipRed(this.physics.canvasWidth / 2, 600, this.resources);
+            break;
+        case 2:
+            spaceship = new SpaceshipGreen(this.physics.canvasWidth / 2, 600, this.resources);
+            break;
+        case 3:
+            spaceship = new SpaceshipBlue(this.physics.canvasWidth / 2, 600, this.resources);
+            break;
+        default:
+            throw new Error('Invalid Starship choose');
+        }
+
+        return spaceship;
+    };
+
+    PlayerFactory.prototype.getRandomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
     };
 
     window.PlayerFactory = PlayerFactory;
