8dc72b743bf9d7021bbbc6a9a9b4583ff84e6f3a
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2015-05-01 19:15:05 +0200

    feat(audio): nouvelle gestion des sons pour mettre le mute.
    - ajout fonction de mute

--- src/audioManager.js
+++ src/audioManager.js
@@ -2,11 +2,9 @@
     'use strict';
 
     function AudioManager(soundDescriptors) {
+        this.sounds = {};
         this.soundDescriptors = soundDescriptors || {};
-        this.soundBuffers = {};
-        this.bufferSources = {};
-        this.loadCount = 0;
-        this.playingSound = false;
+        this.mute = false;
 
         try {
             window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -19,39 +17,48 @@
     AudioManager.prototype.loadSounds = function (soundDescriptors) {
 
         var promises = soundDescriptors.map(function (soundDescriptor) {
-            return this.loadSound(soundDescriptor);
+            var sound = this.sounds[soundDescriptor.title];
+
+            if (!sound) {
+                this.sounds[soundDescriptor.title] = {
+                    title: soundDescriptor.title,
+                    url: soundDescriptor.url
+                };
+                return this.loadSound(this.sounds[soundDescriptor.title]);
+            }
+            return sound;
         }.bind(this));
 
         return Promise.all(promises);
     };
 
-    AudioManager.prototype.loadSound = function (soundDescriptor) {
+    AudioManager.prototype.loadSound = function (sound) {
 
         return new Promise(function (resolve, reject) {
 
-            if (this.soundBuffers[soundDescriptor.title]) {
-                resolve(soundDescriptor);
+            if (sound.buffer) {
+                resolve(sound);
             } else {
                 var request = new XMLHttpRequest();
-                request.open('GET', soundDescriptor.url, true);
+                request.open('GET', sound.url, true);
                 request.responseType = 'arraybuffer';
 
                 request.onload = function () {
                     this.audioContext.decodeAudioData(
                         request.response,
                         function (buffer) {
-                            this.soundBuffers[soundDescriptor.title] = buffer;
-                            resolve(soundDescriptor);
+                            sound.buffer = buffer;
+                            resolve(sound);
                         }.bind(this),
                         function (error) {
-                            console.error('Fail to decodeAudioData [%s]', soundDescriptor.url, error);
-                            reject(soundDescriptor);
+                            console.error('Fail to decodeAudioData [%s]', sound.url, error);
+                            reject(sound);
                         }
                     );
                 }.bind(this);
 
                 request.onerror = function (event) {
-                    reject("Erreur XMLHttpRequest ", event);
+                    reject(new Error("Erreur XMLHttpRequest", event));
                 };
 
                 request.send();
@@ -65,39 +72,49 @@
     };
 
     AudioManager.prototype.stageBgm = function () {
-        return this.play(this.soundBuffers.stage, true, 0, 0);
+        return this.play(this.sounds.stage, true, 0, 0);
     };
 
     AudioManager.prototype.laser = function () {
-        return this.play(this.soundBuffers.laser, false);
+        return this.play(this.sounds.laser, false);
     };
 
-    AudioManager.prototype.mute = function () {
-        this.source.stop(0);
+    AudioManager.prototype.toogleMute = function () {
+        this.mute = !this.mute;
+        if (this.mute) {
+            this.sounds.stage.source.stop(0);
+        } else {
+            this.stageBgm();
+        }
+        return this.mute;
     };
 
     AudioManager.prototype.play = function (sound, loop, loopStart, loopEnd) {
         return new Promise(function (resolve) {
+            if (!this.mute) {
+                var source = this.audioContext.createBufferSource();
+                sound.source = null;
+                sound.source = source;
 
-            var source = this.audioContext.createBufferSource();
-            source.buffer = sound;
-            source.loop = loop;
+                source.buffer = sound.buffer;
+                source.loop = loop;
 
-            if (loop) {
-                source.loopStart = loopStart;
-                source.loopEnd = loopEnd;
+                if (loop) {
+                    source.loopStart = loopStart;
+                    source.loopEnd = loopEnd;
 
-                resolve(source);
-            }
-
-            source.onended = function () {
-                if (!loop) {
                     resolve(source);
                 }
-            };
 
-            source.connect(this.audioContext.destination);
-            source.start(0);
+                source.onended = function () {
+                    if (!loop) {
+                        resolve(source);
+                    }
+                };
+
+                source.connect(this.audioContext.destination);
+                source.start(0);
+            }
         }.bind(this));
     };
 
